9e82a12d3191cd6af0d5ce0ef7d5ecac27437b89
ODSTL <dan@danjaboor.com>
2019-10-27 12:52:28 -0500

    Initial commit

---
+++ src/main/java/com/example/ArtTracker/ArtTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.example.ArtTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtTrackerApplication.class, args);
+	}
+
+
+}
