d18c894bdb831ec5439cb6e604523b4df1f71223
ODSTL <dan@danjaboor.com>
2019-11-05 18:54:35 -0600

    Partial bootstrap implementation

--- src/main/java/com/example/ArtTracker/Controllers/HelloController.java
+++ src/main/java/com/example/ArtTracker/Controllers/HelloController.java
@@ -1,18 +1,46 @@
+package com.bootstrapwithspringboot.webapp.contoller;
 
-package com.example.ArtTracker.Controllers;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Date;
 
 @Controller
+public class HelloContoller {
+    private String appMode;
+
+    @Autowired
+    public HelloContoller(Environment environment){
+        appMode = environment.getProperty("app-mode");
+    }
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Ömerrrr");
+        model.addAttribute("mode", appMode);
+
+        return "index";
+    }
+}
+
+
+//package com.example.ArtTracker.Controllers;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+
+//@Controller
 
 
 
 public class HelloController {
     @RequestMapping(value="")
-    @ResponseBody
+   // @ResponseBody
     public String index(){
-        return "Hello World";
+        return "Hello Dan";
     }
 }
