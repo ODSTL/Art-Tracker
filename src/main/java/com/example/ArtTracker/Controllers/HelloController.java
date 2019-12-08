40d2c72a071acc63835fdf5e1ccaef8933b3acf0
ODSTL <dan@danjaboor.com>
2019-12-08 15:22:31 -0600

    framework adjustment. Starting to outline initial pages to continue build.

--- src/main/java/com/example/ArtTracker/Controllers/HelloController.java
+++ src/main/java/com/example/ArtTracker/Controllers/HelloController.java
@@ -1,18 +1,18 @@
 
-package com.example.ArtTracker.Controllers;
+//package com.example.ArtTracker.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller
+//@Controller
 
 
 
-public class HelloController {
-    @RequestMapping(value="")
-    @ResponseBody
-    public String index(){
-        return "Hello World";
-    }
-}
+//public class HelloController {
+  //  @RequestMapping(value="")
+   // @ResponseBody
+   // public String index(){
+     //   return "JUST KEEP GOING DUDE.";
+   // }
+//}
